4230bca1b5eb4dd60bfc0dd7dd04ab879651c508
DeepAshishThapa <dipashishthapa98@gmail.com>
2026-02-17 22:48:36 +1030

    setup server entry point and database connection

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 
 import connectDB from './db/index.js'
 import dotenv from "dotenv"
+import app from './app.js'
 
 dotenv.config()
 
-connectDB()
+await connectDB()
 
 
+const PORT=process.env.PORT || 8000
+
+app.listen(PORT,()=>{
+    console.log(`server running on port ${PORT}`)
+})
