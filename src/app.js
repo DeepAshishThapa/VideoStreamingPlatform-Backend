93b7a27dbf4249bae641d611b6946cd820ece80e
DeepAshishThapa <dipashishthapa98@gmail.com>
2026-02-20 02:47:28 +1030

    setup core middleware and user routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
+import errorHandler from "./middlewares/error.middleware.js"
 
 const app=express()
 
@@ -13,7 +14,15 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended: true, limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+app.use(errorHandler)
 
+//routes import
+
+import userRouter from "./routes/user.routes.js"
+
+//routes declaration
+
+app.use("/api/v1/users",userRouter)
 
 
 export default app
