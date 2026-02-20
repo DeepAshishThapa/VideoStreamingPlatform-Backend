3129b2b57c9f10f5617280482d90d067ab2e0f26
DeepAshishThapa <dipashishthapa98@gmail.com>
2026-02-21 04:04:57 +1030

    fixed the order of using global middleware

--- src/app.js
+++ src/app.js
@@ -11,10 +11,10 @@ app.use(cors({
 }))
 
 app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended: true, limit:"16kb"}))
+// app.use(express.urlencoded({extended: true, limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
-app.use(errorHandler)
+
 
 //routes import
 
@@ -23,6 +23,7 @@ import userRouter from "./routes/user.routes.js"
 //routes declaration
 
 app.use("/api/v1/users",userRouter)
+app.use(errorHandler)
 
 
 export default app
