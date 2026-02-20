67dee74b6bd03f549c038dede12858c54784e3d1
DeepAshishThapa <dipashishthapa98@gmail.com>
2026-02-21 04:05:29 +1030

    mention path in dotenv config

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 
-import connectDB from './db/index.js'
 import dotenv from "dotenv"
+import connectDB from './db/index.js'
 import app from './app.js'
 
-dotenv.config()
+dotenv.config({
+       path: './.env'
+})
 
 await connectDB()
 
