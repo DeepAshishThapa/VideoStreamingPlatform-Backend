8a0d7de2f577855a9b37625b377e4c5553ff6165
DeepAshishThapa <dipashishthapa98@gmail.com>
2026-02-21 04:06:41 +1030

    resolved path issue

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,10 +1,13 @@
 import multer from "multer";
+import path from "path";
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, "./public/temp")
+      console.log("📦 MULTER DESTINATION HIT");
+      cb(null, path.resolve("public/temp"))
     },
     filename: function (req, file, cb) {
+      console.log("📦 MULTER DESTINATION HIT");
       
       cb(null, file.originalname)
     }
