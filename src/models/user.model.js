3de55dde253358bc1b0b282c1642ccbe1b0830b0
DeepAshishThapa <dipashishthapa98@gmail.com>
2026-02-23 21:51:51 +1030

    removed small typo error

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -94,7 +94,7 @@ userSchema.methods.generateAccessToken = function (){
 userSchema.methods.generateRefreshToken = function (){
     return jwt.sign(
         {
-            _id:this._id,
+            _id:this._id
             
 
         },
