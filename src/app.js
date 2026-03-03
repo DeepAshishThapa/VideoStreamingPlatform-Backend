45388a18a920e55f2ec9a8c842805fb297a472ec
DeepAshishThapa <dipashishthapa98@gmail.com>
2026-03-03 18:34:37 +1030

    fix: correct route declarations for videos, comments, and likes in the API

--- src/app.js
+++ src/app.js
@@ -27,9 +27,9 @@ import likeRouter from "./routes/like.routes.js"
 //routes declaration
 
 app.use("/api/v1/users",userRouter)
-app.use("api/v1/videos",videoRouter)
-app.use("api/v1/comments",commentRouter)
-app.use("api/v1/likes",likeRouter)
+app.use("/api/v1/videos",videoRouter)
+app.use("/api/v1/comments",commentRouter)
+app.use("/api/v1/likes",likeRouter)
 app.use(errorHandler)
 
 
