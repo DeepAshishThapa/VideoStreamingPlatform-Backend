6594ecbc47207a59f2c9de18a95a15621ec8cbca
DeepAshishThapa <dipashishthapa98@gmail.com>
2026-03-03 23:58:23 +1030

    feat: add validation for channel and subscriber IDs in subscription controllers

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -47,11 +47,37 @@ const toggleSubscription = async (req, res) => {
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = async (req, res) => {
     const { channelId } = req.params
+
+    if (!isValidObjectId(channelId)) {
+        throw new ApiError(400, "Invalid channel ID")
+    }
+
+    //  Find all subscriptions for this channel
+    const subscribers = await Subscription.find({ channel: channelId })
+        .populate("subscriber", "username fullname avatar")
+
+    return res.status(200).json(
+        new ApiResponse(200, "Subscribers fetched successfully", subscribers)
+    )
 }
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = async (req, res) => {
     const { subscriberId } = req.params
+
+     if (!isValidObjectId(subscriberId)) {
+        throw new ApiError(400, "Invalid subscriber ID")
+    }
+
+     // Find all channels this user has subscribed to
+    const subscribedChannels = await Subscription.find({
+        subscriber: subscriberId
+    }).populate("channel", "username fullname avatar")
+    
+
+     return res.status(200).json(
+        new ApiResponse(200, "Subscribed channels fetched successfully", subscribedChannels)
+    )
 }
 
 export {
